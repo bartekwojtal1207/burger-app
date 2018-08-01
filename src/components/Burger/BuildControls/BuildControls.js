332f1829cce4e91a3e5a444a8ffa0725acb1c942
bartosz.wojtal <bartosz.wojtal@x-kom.pl>
2018-08-01 14:26:50 +0200

    add build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxs';
+import classes from './BuildControls.css';
+import buildControl from './BuildControl/BuildControl';
 
 
-const buildControls  = {
+const buildControls  = (props) =>  {
+
+    return (
+        <div className={classes.BuildControls}>
+
+        </div>
+    )
+};
 
 
-
-
-}
-
-
-export default BuildControls;
+export default buildControls;
