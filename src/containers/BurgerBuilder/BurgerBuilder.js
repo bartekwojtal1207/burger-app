6519b530eea5f0383d9b02150f4fa9a97c50cafe
bartosz.wojtal <bartosz.wojtal@x-kom.pl>
2018-08-02 08:55:02 +0200

    add build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+
 import Aux from '../../hoc/Auxs';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+};
 
 class BurgerBuilder extends Component {
 
@@ -13,8 +21,23 @@ class BurgerBuilder extends Component {
                 bacon: 0,
                 cheese: 0,
                 meat: 0
-            }
+            },
+            totalPrice: 4
         }
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        // this.setState(ingredients[type], updatedCounted)
+    }
+
+    removeIngredientHandler = (type) => {
+
     }
 
     render() {
