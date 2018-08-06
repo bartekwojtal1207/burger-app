81aca7df9b35973c5b514b015b102f4445103e3d
bartosz.wojtal <bartosz.wojtal@x-kom.pl>
2018-08-06 10:20:45 +0200

    first line to modal js

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,9 +4,11 @@ import Aux from '../../hoc/Auxs';
 import classes from './Modal.css';
 
 const modal = (props) => {
-
-
-
+    return (
+        <div className={classes.Modal}>
+            {props.children}
+        </div>
+    )
 };
 
 
