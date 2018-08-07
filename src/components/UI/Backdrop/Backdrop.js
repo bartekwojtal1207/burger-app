a4d02d70ada857250ad3074ff58f1536c4df6f68
bartosz.wojtal <bartosz.wojtal@x-kom.pl>
2018-08-07 15:23:10 +0200

    showing % hiding the modal with animation !

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
-import Aux from '../../hoc/Auxs';
+import React from 'react';
 
+import classes from './Backdrop.css';
 
 const backdrop = (props) => {
-
+    return (
+        props.show ? <div className={classes.Backdrop} onClick={props.clicked}></div> : null
+    )
 };
 
 
