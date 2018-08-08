faf70c2f43f15b822f06ddff25c1bc86ebe1e010
bartosz.wojtal <bartosz.wojtal@x-kom.pl>
2018-08-08 13:44:28 +0200

    make responsive sidedrawer & toolbar

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -8,8 +8,11 @@ const sideDrawer = (props) => {
 
     return (
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
+                asdasd
                 <NavigationItems/>
             </nav>
 
